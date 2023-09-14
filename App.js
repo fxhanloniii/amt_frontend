4854962e56df9068cd8d4f00913c3b58803a01f2
fxhanloniii <hanlonfrankiex@gmail.com>
2023-09-14 15:15:31 -0700

    setting up app.js

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Screen Imports
+import Home from './screens/Home/Home';
+import SignUp from './screens/SignUp/SignUp';
+import Category from './screens/CategoryItems/CategoryItems';
+import Search from './screens/Search/Search';
+import Item from './screens/Item/Item';
+import LogIn from './screens/LogIn/LogIn';
+
+const Stack = createStackNavigator();
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Another Man's Trash!!!</Text>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="LogIn" component={LogIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="Category" component={Category} />
+        <Stack.Screen name="Search" component={Search} />
+        <Stack.Screen name="Item" component={Item} />
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/CategoryItems/CategoryItems.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function CategoryItems() {
+  return (
+    <View>
+      <Text>CategoryItems</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
