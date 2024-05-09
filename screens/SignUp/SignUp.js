71431427fc4088e7c2f9b8c20d7a9252c645006c
fxhanloniii <hanlonfrankiex@gmail.com>
2024-05-09 10:24:07 -0700

    front end changes

--- screens/SignUp/SignUp.js
+++ screens/SignUp/SignUp.js
@@ -7,6 +7,7 @@ export default function SignUp({ navigation }) {
     const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [zipCode, setZipCode] = useState('');
     const [isPasswordVisible, setPasswordVisibility] = useState(false);
     const [confirmPassword, setConfirmPassword] = useState('');
     const [profileImage, setProfileImage] = useState(null);
@@ -48,9 +49,13 @@ export default function SignUp({ navigation }) {
             const data = await response.json();
             setProfileImage({ uri: data.image_url });
             updateUserProfileImage(data.image_url); 
-                  }
+            } else {
+                Alert.alert('Error', 'Error uploading image. Please try again.');
+                return null;
+            }
         } catch (error) {
-          console.error('Error uploading image:', error);
+          Alert.alert('Error', 'Error uploading image. Please try again.');
+          return null;
         }
       };
     
@@ -76,32 +81,40 @@ export default function SignUp({ navigation }) {
 
     return (
         <View style={styles.container}>
-            <TouchableOpacity onPress={pickImage}>
-                {profileImage ? (
-                    <Image source={{ uri: profileImage }} style={styles.profileImage} />
-                ) : (
-                    <View style={styles.placeholderImage}>
-                        <Text>Add Photo</Text>
-                    </View>
-                )}
-            </TouchableOpacity>
+            <View style={styles.photoContainer}>
+                <TouchableOpacity onPress={pickImage}>
+                    {profileImage ? (
+                        <Image source={{ uri: profileImage }} style={styles.profileImage} />
+                    ) : (
+                        <View>
+                            <View style={styles.placeholderImage}>
+                                
+                            </View>
+                            <Text style={styles.addPhotoText}>Add Photo</Text>
+                        </View>
+                    )}
+                </TouchableOpacity>
+            </View>
             <TextInput
                 style={styles.input}
                 placeholder="Username"
                 onChangeText={text => setUsername(text)}
                 value={username}
             />
+            
             <TextInput
                 style={styles.input}
-                placeholder="First Name"
-                onChangeText={text => setFirstName(text)}
-                value={firstName}
+                placeholder="Password"
+                // secureTextEntry={!isPasswordVisible}
+                onChangeText={text => setPassword(text)}
+                value={password}
             />
             <TextInput
                 style={styles.input}
-                placeholder="Last Name"
-                onChangeText={text => setLastName(text)}
-                value={lastName}
+                placeholder="Confirm Password"
+                // secureTextEntry={!isPasswordVisible}
+                onChangeText={text => setConfirmPassword(text)}
+                value={confirmPassword}
             />
             <TextInput
                 style={styles.input}
@@ -109,27 +122,25 @@ export default function SignUp({ navigation }) {
                 onChangeText={text => setEmail(text)}
                 value={email}
             />
-            <TextInput
+            <TextInput // Add the new TextInput for zipcode
                 style={styles.input}
-                placeholder="Password"
-                secureTextEntry={!isPasswordVisible}
-                onChangeText={text => setPassword(text)}
-                value={password}
+                placeholder="Zipcode"
+                onChangeText={text => setZipCode(text)}
+                value={zipCode}
+                keyboardType="numeric" 
             />
-            <TextInput
-                style={styles.input}
-                placeholder="Confirm Password"
-                secureTextEntry={!isPasswordVisible}
-                onChangeText={text => setConfirmPassword(text)}
-                value={confirmPassword}
-            />
-            <TouchableOpacity 
+            {/* <TouchableOpacity 
                     style={styles.toggleIcon}
                     onPress={() => setPasswordVisibility(prevState => !prevState)}
                 >
                     <Text>{isPasswordVisible ? 'Hide' : 'Show'}</Text>
+            </TouchableOpacity> */}
+            <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+            <View style={styles.buttonSymbol}>
+                <Text style={styles.symbolText}>{'>'}</Text>
+            </View>
+                <Text style={styles.buttonText}>Create Account</Text>
             </TouchableOpacity>
-            <Button title="Sign Up" onPress={handleSignUp} />
         </View>
     );
 }
@@ -142,12 +153,75 @@ const styles = StyleSheet.create({
         backgroundColor: '#f2efe9',
     },
     input: {
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
+        height: 44,
+        borderColor: 'darkgray',
+        borderWidth: 0.5,
+        borderRadius: 25,
         marginBottom: 10,
         padding: 10,
         backgroundColor: '#fcfbfa',
-        // fontFamily: 'basicsans-regularit',
+        fontFamily: 'basicsans-regularit',
+        paddingLeft: 20,
+    },
+    photoContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    addPhotoText: {
+        color: '#293e48',
+        fontSize: 14,
+        fontFamily: 'basicsans-regularit',
+        textAlign: 'center',
+    },
+    profileImage: {
+        width: 120,
+        height: 120,
+        borderRadius: 75,
+        marginBottom: 20,
+    },
+    placeholderImage: {
+        width: 120,
+        height: 120,
+        borderRadius: 75,
+        marginBottom: 5,
+        backgroundColor: '#293e48',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        flexDirection: 'row', 
+        padding: 2,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderRadius: 50,
+        borderWidth: 1,
+        backgroundColor: '#293e48',
+        width: '100%', 
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+        fontFamily: 'basicsans-regular',
+        flex: 1,
+        textAlign: 'center',
+        marginLeft: -10,
+    },
+    buttonSymbol: {
+        width: 36, 
+        height: 36, 
+        borderRadius: 18, 
+        backgroundColor: 'white', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+    },
+    symbolText: {
+        color: '#293e48', 
+        fontSize: 28, 
+        fontFamily: 'basicsans-regular',
+        alignSelf: 'center',
+        paddingTop: 0,
+        lineHeight: 28,
     },
 });
